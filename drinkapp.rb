b981c11f196070fc6318e540a490d7c7ffb16931
Ingrid02 <lezette@bellsouth.net>
2017-10-28 22:28:55 -0400

    add additional logic and readme file

--- drinkapp.rb
+++ drinkapp.rb
@@ -1,3 +1,6 @@
+
+
+
 puts "Would you like water or wine?"
 
 print "> "
@@ -7,15 +10,35 @@ if bev == "wine"
     puts "Would you like red or white?"
     print "> "
     winetype = $stdin.gets.chomp
-else bev == "water"
+        if winetype == "red"
+            puts "Please select Pinot Noir or Merlot"
+            print "> "
+            redwinetype = $stdin.gets.chomp
+            puts "Enjoy your %s "%bev
+        elsif winetype =="white"
+            puts "Please select Chardonnay or Sauvignon Blanc"
+            print "> "
+            whitewinetype = $stdin.gets.chomp
+            puts "Enjoy your %s "%bev 
+        else 
+            puts "Sorry we do not have that selection at the moment"
+            end
+elsif bev == "water"
     puts "Would you like sparkling or still?"
     print "> "
     watertype = $stdin.gets.chomp
     puts "Enjoy your %s "%bev
+else 
+    puts "That is not a valid selection"
 
 end  
+    
 
 #if wine then ask "red or white"
+    #if "red" wine, ask "pinot noir or merlot"
+    #if "white" wine, ask "chardonnay or Sauvignon blanc"
+    #if neither, respond "Sorry we do not have that selection at the moment"
 #if water then ask "sparkling or flat"
+#if neither water or wine selected, respond "that's invalid selection"
 
 
---
+++ readme.md
@@ -0,0 +1,8 @@
+# drinkapp
+wine or water app - project for the SheNomads mentorship program
+
+Run app in the command line:
+ruby drinkapp.rb
+
+Purpose of app
+select whether user wants water or wine.  If wine the user selects red or white.  If water, the user selects sparkling or still. input and results will appear in the command line
